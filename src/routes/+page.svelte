d21351e897dad802e9b357570b7b989aea61ed37
Hiroaki Yutani <yutani@mierune.co.jp>
2025-05-23 16:42:56 +0900

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,50 @@
   import "maplibre-gl/dist/maplibre-gl.css";
   import { onMount } from "svelte";
 
+  const origMaxHeight = 3000;
+  const makeColorRelief = (
+    maxHeight: number
+  ): maplibregl.ExpressionSpecification => {
+    const scale = maxHeight / origMaxHeight;
+
+    // prettier-ignore
+    return [
+      "interpolate",
+      ["linear"],
+      ["elevation"],
+      // original code: https://github.com/NathanMOlson/NathanMOlson.github.io/blob/9fccba3165751dea4253c38c389e99494e43e83a/color_relief/index.html#L46-L59
+      // 
+      // 地理院標高タイルの換算は、10倍して10,000を引く
+      // cf. https://zenn.dev/shi_works/articles/6f50506d5bc3b0
+         0   * scale * 10 - 10000, "#c0e0ffff",
+         0.1 * scale * 10 - 10000, "#d8d8d8ff",
+         3   * scale * 10 - 10000, "#548359ff",
+       255   * scale * 10 - 10000, "#32482dff",
+      1000   * scale * 10 - 10000, "#32482dff",
+      1200   * scale * 10 - 10000, "#adb7a3ff",
+      1700   * scale * 10 - 10000, "#bfbfb8ff",
+      1750   * scale * 10 - 10000, "#e6f4fdff",
+      3000   * scale * 10 - 10000, "#ffffffff"
+    ];
+  };
+
+  let map: maplibregl.Map;
+
+  let scale = $state(origMaxHeight);
+  $effect(() => {
+    scale;
+    console.log(map);
+    if (map) {
+      map.setPaintProperty(
+        "color_relief",
+        "color-relief-color",
+        makeColorRelief(scale)
+      );
+    }
+  });
+
   onMount(() => {
-    const map = new maplibregl.Map({
+    map = new maplibregl.Map({
       container: "map",
       style: {
         version: 8,
@@ -41,23 +83,7 @@
             source: "terrain",
             paint: {
               "color-relief-opacity": 0.8,
-              // prettier-ignore
-              "color-relief-color": [
-                "interpolate",
-                ["linear"],
-                ["elevation"],
-                // 地理院標高タイルの換算は、10倍して10,000を引く
-                // cf. https://zenn.dev/shi_works/articles/6f50506d5bc3b0
-                -10000, "#c0e0ffff",
-                -9999, "#d8d8d8ff",
-                -9970, "#548359ff",
-                -7450, "#32482dff",
-                0, "#32482dff",
-                2000, "#adb7a3ff",
-                7000, "#bfbfb8ff",
-                7500, "#e6f4fdff",
-                20000, "#ffffffff"
-              ],
+              "color-relief-color": makeColorRelief(origMaxHeight),
             },
           },
         ],
@@ -66,4 +92,23 @@
   });
 </script>
 
-<div id="map" style="height: 100vh"></div>
+<div style="height: 4vh">
+  最大標高：
+  <label>
+    <input
+      type="number"
+      bind:value={scale}
+      min="1"
+      max={origMaxHeight * 2}
+      step="1"
+    />
+    <input
+      type="range"
+      bind:value={scale}
+      min="1"
+      max={origMaxHeight * 2}
+      step="1"
+    />
+  </label>
+</div>
+<div id="map" style="height: 96vh"></div>
